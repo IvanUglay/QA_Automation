ba0c8de014f035d1e0913404b154a63995e769f9
Uhliai <49193325+IvanUglay@users.noreply.github.com>
2025-11-30 21:14:20 +0100

    Delete src/lesson14/vitest.config.ts

--- src/lesson14/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-    test: {
-        exclude: [],
-        include: ['./tests/**/?(*.)+(spec|test).[t]s?(x)'],
-        testTimeout: 120000
-    }
-});
